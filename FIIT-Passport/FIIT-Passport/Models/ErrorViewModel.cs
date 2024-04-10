c4ad18dfb5b97f5e3d587adff74655b3fb4a4fb9
ninimimm <nikitachernov89221156246@yandex.ru>
2024-04-10 08:54:45 +0500

    change to mvs model

---
+++ FIIT-Passport/FIIT-Passport/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Fiit_passport.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
