e2fd6c349a605f2d2c3f6712b9c42509c6831de9
Nikita Chernov <nikitachernov89221156246@yandex.ru>
2024-05-21 21:03:23 +0500

    change host

--- FIIT-Passport/FIIT-Passport/Fiit-passport/Program.cs
+++ FIIT-Passport/FIIT-Passport/Fiit-passport/Program.cs
@@ -27,16 +27,25 @@ var host = new WebHostBuilder()
     {
         options.ListenAnyIP(8888);
     })
-    .ConfigureServices(services =>
+    .ConfigureServices((context, services) =>
     {
-        services.AddCors();
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
+
         services.AddScoped<TelegramDbContext>();
         services.AddScoped<TelegramBot>();
         services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(
-            builder.Configuration.GetConnectionString("DefaultConnection")
+            context.Configuration.GetConnectionString("DefaultConnection")
         ));
         services.AddSingleton<ITelegramBotClient>(_ =>
-            new TelegramBotClient("6599160966:AAEPk4mP04rI5jzHQJr65a4xlyHRQIUCygk"));
+            new TelegramBotClient("YOUR_BOT_TOKEN"));
         services.AddControllers();
     })
     .Configure(applicationBuilder =>
@@ -45,11 +54,11 @@ var host = new WebHostBuilder()
         {
             applicationBuilder.UseDeveloperExceptionPage();
         }
-        applicationBuilder.UseCors(builder => 
-            builder.AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader());
+
+        applicationBuilder.UseCors();
+
         applicationBuilder.UseRouting();
+
         applicationBuilder.UseEndpoints(endpoints =>
         {
             endpoints.MapControllers();
